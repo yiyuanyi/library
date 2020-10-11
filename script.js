63ba17569658f7c1c286eb842ea89a663174b4ad
Matt H <59845559+yiyuanyi@users.noreply.github.com>
2020-10-11 15:13:27 +0100

    add books to myLibrary array. add code to Book() function.

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-let myLibrary = [];
+let myLibrary = [
+    {
+        title: 'A Little Life',
+        author: 'Hanya Yanagihara',
+        year: 2015,
+        status: 'read'
+    }, 
+    {
+        title: 'Dance Dance Dance',
+        author: 'Haruki Murakami',
+        year: 1988,
+        status: 'read'
+    },
+    {
+        title: 'The Wolf of Wall Street',
+        author: 'Jordan Belfort',
+        year: 2007,
+        status: 'not read'
+    }
+
+];
 
 function Book() {
-
+    this.title = title,
+    this.author = author,
+    this.year = year,
+    this.status = status
 }
 
 function addBookToLibrary() {
-    
+
 }
